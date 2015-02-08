73fe4af6b848b82fc31937cabec8df5e2d45e0a2
Fabien Serny <fabien@23prod.fr>
2015-02-08 15:40:29 +0100

    Add translations

---
+++ es.php
@@ -0,0 +1,33 @@
+<?php
+
+global $_MODULE;
+
+$_MODULE = array();
+$_MODULE['<{froggyhomecategories}prestashop>froggyhomecategories_b5de5ab2bb73d99dcd40e47580b769d1'] = 'Froggy Categorías sobre la Acogida';
+$_MODULE['<{froggyhomecategories}prestashop>froggyhomecategories_835b431646ea32c2013d6e9ba7f283bb'] = 'Le permite fijar una selección de categorías sobre la acogida';
+$_MODULE['<{froggyhomecategories}prestashop>froggyhomecategories_254f642527b45bc260048e30704edb39'] = 'Configuración';
+$_MODULE['<{froggyhomecategories}prestashop>froggyhomecategories_dae8ace18bdcbcc6ae5aece263e14fe8'] = 'Opciones';
+$_MODULE['<{froggyhomecategories}prestashop>froggyhomecategories_8505151a07c0b2387f139bb4d15e3093'] = 'Categorías seleccionadas:';
+$_MODULE['<{froggyhomecategories}prestashop>displayhome.bootstrap_4351cfebe4b61d8aa5efa1d020710005'] = 'Ver';
+$_MODULE['<{froggyhomecategories}prestashop>displayhome.bootstrap_218fda0ee7e482c1385891a68175d79f'] = 'Ningunas subcategorías para esta categoría';
+$_MODULE['<{froggyhomecategories}prestashop>displayhome_c55d773908d0869c3043077002c39e54'] = 'Las categorías';
+$_MODULE['<{froggyhomecategories}prestashop>displayhome_4351cfebe4b61d8aa5efa1d020710005'] = 'Ver';
+$_MODULE['<{froggyhomecategories}prestashop>displayhome_218fda0ee7e482c1385891a68175d79f'] = 'Ningunas subcategorías para esta categoría';
+$_MODULE['<{froggyhomecategories}prestashop>getcontent.bootstrap_b5de5ab2bb73d99dcd40e47580b769d1'] = 'Froggy Categorías sobre la Acogida';
+$_MODULE['<{froggyhomecategories}prestashop>getcontent.bootstrap_4725c2b6db34f755b89b741c1d45bc71'] = '¡ La nueva configuración ha sido bien salvaguardada !';
+$_MODULE['<{froggyhomecategories}prestashop>getcontent_b5de5ab2bb73d99dcd40e47580b769d1'] = 'Froggy Categorías sobre la Acogida';
+$_MODULE['<{froggyhomecategories}prestashop>getcontent_4725c2b6db34f755b89b741c1d45bc71'] = '¡ La nueva configuración ha sido bien salvaguardada !';
+$_MODULE['<{froggyhomecategories}prestashop>helper-form.bootstrap_93cba07454f06a4a960172bbd6e2a435'] = 'Sí';
+$_MODULE['<{froggyhomecategories}prestashop>helper-form.bootstrap_bafd7322c6e97d25b6299b5d6fe8920b'] = 'No';
+$_MODULE['<{froggyhomecategories}prestashop>helper-form.bootstrap_c9cc8cce247e49bae79f15173ce97354'] = 'salvaguardar';
+$_MODULE['<{froggyhomecategories}prestashop>helper-form_93cba07454f06a4a960172bbd6e2a435'] = 'Sí';
+$_MODULE['<{froggyhomecategories}prestashop>helper-form_bafd7322c6e97d25b6299b5d6fe8920b'] = 'No';
+$_MODULE['<{froggyhomecategories}prestashop>helper-form_c9cc8cce247e49bae79f15173ce97354'] = 'salvaguardar';
+$_MODULE['<{froggyhomecategories}prestashop>helper-list.bootstrap_06df33001c1d7187fdd81ea1f5b277aa'] = 'Acciones';
+$_MODULE['<{froggyhomecategories}prestashop>helper-list.bootstrap_f2a6c498fb90ee345d997f888fce3b18'] = 'Borrar';
+$_MODULE['<{froggyhomecategories}prestashop>helper-list_06df33001c1d7187fdd81ea1f5b277aa'] = 'Acciones';
+$_MODULE['<{froggyhomecategories}prestashop>helper-list_f2a6c498fb90ee345d997f888fce3b18'] = 'Borrar';
+$_MODULE['<{froggyhomecategories}prestashop>helper-tree.bootstrap_4c41e0bd957698b58100a5c687d757d9'] = 'Seleccionar todo';
+$_MODULE['<{froggyhomecategories}prestashop>helper-tree.bootstrap_237c7b6874386141a095e321c9fdfd38'] = 'Deseleccionar todo';
+$_MODULE['<{froggyhomecategories}prestashop>helper-tree_4c41e0bd957698b58100a5c687d757d9'] = 'Seleccionar todo';
+$_MODULE['<{froggyhomecategories}prestashop>helper-tree_237c7b6874386141a095e321c9fdfd38'] = 'Deseleccionar todo';
