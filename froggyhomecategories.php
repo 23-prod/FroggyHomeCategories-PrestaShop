438e88d0e8aed00469c5ef6a91ce3e63c72345c1
Fabien Serny <fabien@23prod.fr>
2015-03-08 15:38:05 +0100

    Update module version + comments

--- froggyhomecategories.php
+++ froggyhomecategories.php
@@ -1,27 +1,32 @@
 <?php
-/*
-* 2013-2015 Froggy Commerce
-*
-* NOTICE OF LICENSE
-*
-* You should have received a licence with this module.
-* If you didn't buy this module on Froggy-Commerce.com, ThemeForest.net
-* or Addons.PrestaShop.com, please contact us immediately : contact@froggy-commerce.com
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to benefit the updates
-* for newer PrestaShop versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author Froggy Commerce <contact@froggy-commerce.com>
-*  @copyright  2013-2015 Froggy Commerce
-*/
+/**
+ * 2013-2015 Froggy Commerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * You should have received a licence with this module.
+ * If you didn't buy this module on Froggy-Commerce.com, ThemeForest.net
+ * or Addons.PrestaShop.com, please contact us immediately : contact@froggy-commerce.com
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to benefit the updates
+ * for newer PrestaShop versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Froggy Commerce <contact@froggy-commerce.com>
+ * @copyright 2013-2015 Froggy Commerce
+ * @license   Unauthorized copying of this file, via any medium is strictly prohibited
+ */
 
-// Security
+/*
+ * Security
+ */
 defined('_PS_VERSION_') || require dirname(__FILE__).'/index.php';
 
-// Include Froggy Library
+/*
+ * Include Froggy Library
+ */
 if (!class_exists('FroggyModule', false)) require_once dirname(__FILE__).'/froggy/FroggyModule.php';
 
 class FroggyHomeCategories extends FroggyModule
@@ -37,7 +42,7 @@ class FroggyHomeCategories extends FroggyModule
 	public function __construct()
 	{
 		$this->name = 'froggyhomecategories';
-		$this->version = '1.0.0';
+		$this->version = '1.0.1';
 		$this->author = 'Froggy Commerce';
 		$this->tab = 'front_office_features';
 
@@ -56,7 +61,15 @@ class FroggyHomeCategories extends FroggyModule
 		return $this->hookGetContent(array());
 	}
 
-	// Retrocompat 1.4
-	public function hookHeader($params) { return $this->hookDisplayHeader($params); }
-	public function hookHome($params) { return $this->hookDisplayHome($params); }
+	/*
+	 * Retrocompat 1.4
+	 */
+	public function hookHeader($params)
+	{
+		return $this->hookDisplayHeader($params);
+	}
+	public function hookHome($params)
+	{
+		return $this->hookDisplayHome($params);
+	}
 }
